739170d507e66316935b39a54011130db9749166
cylee <twimclee@gmail.com>
2025-04-18 18:51:50 +0900

    efficientnet for moai

--- export.py
+++ export.py
@@ -53,6 +53,8 @@ torch.onnx.export(model,
 	training=torch.onnx.TrainingMode.EVAL,
 	do_constant_folding=True,
 	export_params=True,
+    input_names=['images'],
+    output_names=['output0'],
 	verbose=False,)
 
 model = onnx.load(SAVE)
